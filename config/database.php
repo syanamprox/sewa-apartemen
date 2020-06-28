cdc8f5366a2eddc21365064f56a2a27257ca47fd
Syaiful Anam <syanam.pro@gmail.com>
2020-06-29 04:42:23 +0700

    d

--- config/database.php
+++ config/database.php
@@ -44,6 +44,8 @@ return [
             ],
             'port' => env('DB_PORT', 27017),
             'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
             // 'dsn' => env('MONGO_DB_DSN'),
             'options' => [
                 // 'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
